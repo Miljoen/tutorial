a5ae31bc6f4888726cad2c10521b137dfd61a7cb
Yoeri Joëll Moens <yoerimoens@gmail.com>
2025-02-12 17:27:59 +0100

    Added student service

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -4,6 +4,8 @@ import six
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
 
+from swagger_server.service.student_service import *
+
 
 def add_student(body=None):  # noqa: E501
     """Add a new student
@@ -30,7 +32,7 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: object
     """
-    return 'do some magic!'
+    return delete(student_id)
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +45,4 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    return get_student_by_id(student_id)
